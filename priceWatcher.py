8adfb712468a4e12a44e7d04de021f1b80000e50
Kenny Grannas <s526521@nwmissouri.edu>
2021-03-17 15:25:24 -0500

    Amazon Scrapping Struggles

--- priceWatcher.py
+++ priceWatcher.py
@@ -6,25 +6,48 @@ Created on Tue Mar 16 23:30:38 2021
 """
 #Followed example from towards data science article Scraping Amazon Stores to Generate Price Alerts (https://towardsdatascience.com/scraping-multiple-amazon-stores-with-python-5eab811453a8)
 
-#Imported Packages
-import requests
-from glob import glob
-from bs4 import BeautifulSoup
-import pandas as pd
-from datetime import datetime
+from selectorlib import Extractor
+import requests 
+import json 
 from time import sleep
 
-# http://www.networkinghowtos.com/howto/common-user-agent-list/
-HEADERS = ({'User-Agent':
-            'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-            'Accept-Language': 'en-US, en;q=0.5'})
-    
-# imports a csv file with the url's to scrape
-prod_tracker = pd.read_csv('products/Products_To_Track.csv', sep=',')
-prod_tracker_URLS = prod_tracker.url
 
-# fetch the url
-page = requests.get(prod_tracker_URLS[0], headers=HEADERS)
+# Create an Extractor by reading from the YAML file
+e = Extractor.from_yaml_file('selectors.yml')
 
-# create the object that will contain all the info in the url
-soup = BeautifulSoup(page.content, features="lxml")
+def scrape(url):    
+    headers = {
+        'authority': 'www.amazon.com',
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'none',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-dest': 'document',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
+
+    # Download the page using requests
+    print("Downloading %s"%url)
+    r = requests.get(url, headers=headers)
+    # Simple check to check if page was blocked (Usually 503)
+    if r.status_code > 500:
+        if "To discuss automated access to Amazon data please contact" in r.text:
+            print("Page %s was blocked by Amazon. Please try using better proxies\n"%url)
+        else:
+            print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
+        return None
+    # Pass the HTML of the page and create 
+    return e.extract(r.text)
+
+# product_data = []
+with open("amazonTracker.txt",'r') as urllist, open('output.jsonl','w') as outfile:
+    for url in urllist.readlines():
+        data = scrape(url) 
+        if data:
+            json.dump(data,outfile)
+            outfile.write("\n")
+            # sleep(5)
